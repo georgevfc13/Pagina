02b558c05852fa867fd9659dea209746daf0d97a
georgevfc13 <georgeyeste@gmail.com>
2025-08-05 23:12:06 -0500

    .

--- script.js
+++ script.js
@@ -195,3 +195,19 @@ new Chart(document.getElementById("graficoServiciosOfertados"), {
 
     calendar.render();
   });
+
+//boton de scroll
+// Mostrar/ocultar el botón al hacer scroll
+window.addEventListener('scroll', function () {
+  const btn = document.getElementById('scrollToTopBtn');
+    if (window.scrollY > 200) {
+      btn.style.display = 'flex';
+    } else {
+      btn.style.display = 'none';
+      }
+    });
+
+// Scroll suave al inicio al hacer clic
+document.getElementById('scrollToTopBtn').addEventListener('click', function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
