951ff2a912cae211a24d8abe85db85b9cfbbcdaa
auryduarte17 <raulrios1120@gmail.com>
2025-09-01 17:02:33 -0500

    R
    Falta consulta de servicio

--- resultados.php
+++ resultados.php
@@ -17,7 +17,7 @@ $busqueda  = isset($_GET['busqueda']) ? $_GET['busqueda'] : '';
 $tipo      = isset($_GET['tipo']) ? $_GET['tipo'] : '';
 $ubicacion = isset($_GET['ubicacion']) ? $_GET['ubicacion'] : '';
 
-// Construir consulta
+// Construir consulta vacantes
 $sql = "SELECT * FROM vacantes WHERE 1=1";
 
 if (!empty($busqueda)) {
@@ -35,7 +35,27 @@ if (!empty($ubicacion) && $ubicacion != "Ubicación") {
 
 
 $result = $conn->query($sql);
+
+
+
+$sql_servicios = "SELECT * FROM servicios WHERE 1=1";
+
+if (!empty($busqueda)) {
+    $sql_servicios .= " AND (titulo LIKE '%$busqueda%' OR descripcion LIKE '%$busqueda%')";
+}
+if (!empty($tipo) && $tipo != "Tipo") {
+    $sql_servicios .= " AND tipo = '$tipo'";
+}
+if (!empty($ubicacion) && $ubicacion != "Ubicación") {
+    $sql_servicios .= " AND ubicacion = '$ubicacion'";
+}
+
+$result_servicios = $conn->query($sql_servicios);
+
 ?>
+
+
+
 
 <!DOCTYPE html>
 <html lang="es">
@@ -43,9 +63,19 @@ $result = $conn->query($sql);
   <meta charset="UTF-8">
   <title>Resultados de búsqueda</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/styles/resultados.css">
 </head>
-<body class="container mt-5">
-  <h1>Resultados de búsqueda</h1>
+<body class="container mt-5 body">
+
+ <!-- Video de fondo -->
+  <video autoplay muted loop class="video-fondo">
+    <source src="assets/video/empresav.mp4" type="video/mp4">
+    Tu navegador no soporta videos HTML5.
+  </video>
+
+  <!-- Overlay oscuro opcional -->
+  <div class="overlay"></div>
+  <h1 class="text-white">Resultados de búsqueda</h1>
   <hr>
 
   <?php
@@ -58,7 +88,7 @@ $result = $conn->query($sql);
           echo "</div>";
       }
   } else {
-      echo "<p>No se encontraron resultados.</p>";
+      echo " <div class='card mb-3 p-3'> <p>No se encontraron resultados.</p> </div>";
   }
 
   $conn->close();
